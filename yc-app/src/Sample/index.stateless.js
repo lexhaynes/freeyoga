7261e8faf7f2fa09fc4fa75946d6776a95b201bb
Alex Haynes <alex@Alex.local>
2017-01-03 14:12:08 -0500

    added command line component generator; added legal pages to public dir

--- yc-app/src/Sample/index.stateless.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-export default functon Sample(props) {
-	//const { prop1, prop2 } = props;
-	return (
-		<div className = "Sample">Sample</div>
-	)
-}
