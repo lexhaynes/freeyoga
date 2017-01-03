0e90fbe716cb42c5add5e845173cd2d4dc96d20c
Alex Haynes <alex@Alex.local>
2017-01-03 13:19:18 -0500

    added sample components

--- yc-app/src/App/index.js
+++ yc-app/src/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './style.css';
+import './styles.css';
 
 class App extends Component {
   render() {
---
+++ yc-app/src/Sample/index.stateless.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+export default functon Sample(props) {
+	//const { prop1, prop2 } = props;
+	return (
+		<div className = "Sample">Sample</div>
+	)
+}
