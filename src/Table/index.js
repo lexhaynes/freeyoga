aab5d6ef34aefe065682d26afd0335c53f79489f
Alex Haynes <alex@Alex.local>
2017-01-04 11:43:31 -0500

    no longer displaying table data in actual tables for ease of styling

--- src/Table/index.js
+++ src/Table/index.js
@@ -5,21 +5,37 @@ export default function Table(props) {
 	const { className, data } = props;
 	let rendered = data();
 	let parsed = rendered.map(function(timeslot, i) {
-		console.log(timeslot)
-		return <tr key={i}>
+		//console.log(timeslot)
+		/*return <tr key={i}>
 				<th>{timeslot[0]}</th>
 				{ timeslot[1].map(function(item, i) {
 					return <td key={i}>{item}</td>
 				})
 				}
-				</tr>
-	})
-	return (
+				</tr>	*/
+		return (
+			<div className="flex-container" key={i}>
+				<strong>{timeslot[0]}</strong>
+				<div className="flex-container">
+				{ timeslot[1].map(function(item, i) {
+					return <span key={i}>{item}</span>
+				})
+				}
+				</div>
+			</div>
+		)
+	});
+	/*return (
 		<table className={className}>
 			  <tbody>
 			     {parsed} 
 			  </tbody>
 		</table>
+	)*/
+	return (
+		<div className={className}>
+			{parsed}
+		</div>
 	)
 }
 
