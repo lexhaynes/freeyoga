82d9403d8df7154b0fffe749ed691f375bc4c10c
Alex Haynes <alex@Alex.local>
2017-01-05 07:49:41 -0500

    styles

--- src/ClassBox/index.js
+++ src/ClassBox/index.js
@@ -11,11 +11,11 @@ export default function ClassBox(props) {
 
 	  		<a href={url} className="class-location" target="blank">{location}</a>
 
-	  		<div className="class-field"><strong className="label">Neighborhood :</strong> {hood}</div>
+	  		<div className="class-field"><strong className="label">Neighborhood:</strong> {hood}</div>
 
-	  		<div className="class-field"><strong className="label">Class Duration :</strong>{moment(start.replace(/:/g,''), "hmm").format("h:mm a")} - {moment(end.replace(/:/g,''), "hmm").format("h:mm a")}</div>
+	  		<div className="class-field"><strong className="label">Class Duration:</strong>{moment(start.replace(/:/g,''), "hmm").format("h:mm a")} - {moment(end.replace(/:/g,''), "hmm").format("h:mm a")}</div>
 
-	  		<div className="class-field"><strong className="label">Teacher :</strong>  {teacher}</div>
+	  		<div className="class-field"><strong className="label">Teacher:</strong>  {teacher}</div>
 
 	  		<a href={"https://www.google.com/maps/place/" + location  + "/@" + coords + ",13z"} className="class-location" target="blank">See on Map</a>
 	  	</div>;
