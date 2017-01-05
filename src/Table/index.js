c9bda64048511405c06cf8cee33129e5785dda38
Alex Haynes <alex@Alex.local>
2017-01-05 09:36:22 -0500

    made grid prettier

--- src/Table/index.js
+++ src/Table/index.js
@@ -6,15 +6,18 @@ export default function Table(props) {
 	const { className, data } = props;
 	let rendered = data();
 	let parsed = rendered.map(function(timeslot, i) {
+			
+			let flexBasis = timeslot[1].length > 1 ? "flex-50" : "flex-100";
+
 		return (
 			<div className="flex-container row" key={i}>
 				<div className="timeslot">
 					{moment(timeslot[0].replace(/:/g,''), "hmm").format("h:mm a") }
 				</div>
-				<div className="flex-container grid-100">
-				{ timeslot[1].map(function(item, i) {
-					return <div className="grid-100" key={i}>{item}</div>
-				})
+				<div className="flex-container flex-wrap grid-100">
+				{	timeslot[1].map(function(item, y) {
+						return <div className={flexBasis} key={y}>{item}</div>
+					})
 				}
 				</div>
 			</div>
